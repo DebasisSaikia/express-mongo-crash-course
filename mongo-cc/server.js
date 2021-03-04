b997df3a104796bcd73b4c1866d85a6291cc47e9
Debasis Saikia <debasishsaikia136@gmail.com>
2021-03-04 23:11:27 +0530

    express-connection

--- mongo-cc/server.js
+++ mongo-cc/server.js
@@ -3,7 +3,27 @@ const app = express();
 
 const PORT = process.env.port || 3100;
 
+// db connection
+const MongoClient = require("mongodb").MongoClient;
+
+let db;
+
+MongoClient.connect("mongodb://localhost:27017/", function (err, client) {
+  if (err) throw err;
+
+   db = client.db("ecom");
+});
+
+
 app.get("/", (req, res) => {
+
+  db.collection("products")
+    .find()
+    .toArray(function (err, result) {
+      if (err) throw err;
+
+      console.log(result);
+    });
   res.send("Hello World");
 });
 
